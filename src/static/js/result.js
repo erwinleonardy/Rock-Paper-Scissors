b63e0e53b09d2c92485e427143097209b57ae62f
Erwin <35572058+erwinleonardy@users.noreply.github.com>
2019-07-02 23:50:43 +0800

    Update result.js (Added window onload)

--- src/static/js/result.js
+++ src/static/js/result.js
@@ -6,25 +6,27 @@
  *             computer(s) and (or) player
  */
 
-function showResult (playerMove, opponentMove, result) 
-{
-    // updated oponent's picture
-    var opponents = document.getElementsByClassName("opp");
-    for (var i = 0; i < opponents.length; i++)
+window.onload(function(){
+    function showResult (playerMove, opponentMove, result) 
     {
-        if (opponents[i].id != opponentMove)
-            opponents[i].style.opacity = "0.5";
-        else
-            opponents[i].src = "/static/img/" + opponents[i].id + "_Selected_Inverted.png";
-    }
+        // updated oponent's picture
+        var opponents = document.getElementsByClassName("opp");
+        for (var i = 0; i < opponents.length; i++)
+        {
+            if (opponents[i].id != opponentMove)
+                opponents[i].style.opacity = "0.5";
+            else
+                opponents[i].src = "/static/img/" + opponents[i].id + "_Selected_Inverted.png";
+        }
 
-    // updated player's picture
-    var players = document.getElementsByClassName("player");
-    for (var i = 0; i < players.length; i++)
-    {
-        if (players[i].id != playerMove)
-            players[i].style.opacity = "0.5";
-        else
-            players[i].src = "/static/img/" + players[i].id + "_Selected.png";
+        // updated player's picture
+        var players = document.getElementsByClassName("player");
+        for (var i = 0; i < players.length; i++)
+        {
+            if (players[i].id != playerMove)
+                players[i].style.opacity = "0.5";
+            else
+                players[i].src = "/static/img/" + players[i].id + "_Selected.png";
+        }
     }
 }
